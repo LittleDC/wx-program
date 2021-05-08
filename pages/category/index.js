c929e9359d455de55b4c711371099be9a966f04f
LittleDC <1187061276@qq.com>
2021-05-08 21:36:06 +0800

    static cates done

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,37 @@
-// pages/category/index.js
+import {
+  request
+} from "../../request/index.js"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    //左侧菜单数据
+    leftMenuList: [],
+    //右侧商品数据
+    rightContent: [],
+    //被点击的左侧菜单
+    currentIndex: 0
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  //接口的返回数据
+  Cates: [],
+
   onLoad: function (options) {
-
+    this.getCates();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //获取分类数据
+  getCates() {
+    request({
+      url: "https://api-hmugo-web.itheima.net/api/public/v1/categories"
+    }).then(res => {
+      // console.log(res);
+      this.Cates = res.data.message
+      let leftMenuList = this.Cates.map(v => v.cat_name)
+      let rightContent = this.Cates[0].children
+      this.setData({
+        // leftMenuList:leftMenuList
+        leftMenuList,
+        rightContent
+      })
+    })
   }
 })
