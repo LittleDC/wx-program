3de1a7cfddbee0f5f8b1866992260a8b603cfbee
LittleDC <1187061276@qq.com>
2021-05-11 17:40:52 +0800

    goods_details done ^

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,57 @@
 // pages/goods_detail/index.js
+import {
+  request
+} from "../../request/index.js"
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsObj: {}
   },
+  GoodsInfo: {},
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const {
+      goods_id
+    } = options;
+    // console.log(goods_id);
+    this.getGoodsDetails(goods_id);
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //获取商品的详情数据
+  async getGoodsDetails(goods_id) {
+    const res = await request({
+      url: "/goods/detail",
+      data: {
+        goods_id
+      }
+    });
+    this.GoodsInfo = res.data.message;
+    // console.log(res.data.message);
+    this.setData({
+      goodsObj: {
+        goods_name: res.data.message.goods_name,
+        goods_price: res.data.message.goods_price,
+        goods_introduce: res.data.message.goods_introduce,
+        pics: res.data.message.pics,
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //点击轮播图 放大预览
+  handlePreviewImage(e) {
+    // console.log("preview");
+    const urls = this.GoodsInfo.pics.map(v => v.pics_mid);
+    const current = e.currentTarget.dataset.url;
+    wx.previewImage({
+      urls: urls,
+      current: current,
+    })
   }
 })
