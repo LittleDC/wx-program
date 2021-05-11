d95c7371fb451f847077096ee5776dff6fa2aa51
LittleDC <1187061276@qq.com>
2021-05-11 18:59:47 +0800

    get address done

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,13 +1,22 @@
 // pages/cart/index.js
+import regeneratorRuntime from '../../lib/runtime/runtime';
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    address: {},
   },
 
+  onShow() {
+    let address = wx.getStorageSync('address');
+    address.all = address.provinceName + address.cityName + address.countyName + address.detailInfo
+    this.setData({
+      address
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -15,52 +24,18 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  async handleChooseAddress() {
+    // console.log("chat");
+    wx.chooseAddress({
+      success: (result) => {
+        // console.log(result);
+        wx.setStorageSync('address', result)
+      },
+    });
+    // wx.getSetting({
+    //   success: (result1) => {
+    //     console.log(result1)
+    //   }
+    // })
   }
 })
